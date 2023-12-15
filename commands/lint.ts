ea55e6981288bb82200b9ba53e7d14fac75bb409
libmartinito <lib.martinito@pm.me>
2023-12-15 22:00:34 +0800

    CC-866 added javascript and markdown linting in lint command

--- commands/lint.ts
+++ commands/lint.ts
@@ -6,6 +6,10 @@ export default class LintCommand extends BaseCommand {
     super();
   }
 
+  private async dockerShellCommandExecutor(): Promise<DockerShellCommandExecutor> {
+    return new DockerShellCommandExecutor("./", "js-tools");
+  }
+
   async doRun() {
     console.log("Building js-tools Docker image...");
     console.log("");
@@ -13,6 +17,24 @@ export default class LintCommand extends BaseCommand {
     console.log("");
     console.log("js-tools Docker image built.");
     console.log("");
+
+    console.log("Linting JavaScript files...")
+    await this.lintJavaScriptFiles();
+    console.log("Linting of JavaScript files complete.");
+
+    console.log("Linting Markdown files...")
+    await this.lintMarkdownFiles();
+    console.log("Linting of Markdown files complete.");
+  }
+
+  async lintJavaScriptFiles() {
+    const dockerShellCommandExecutor = await this.dockerShellCommandExecutor();
+    await dockerShellCommandExecutor.exec(`prettier --write --ignore-path ./.prettierignore --no-error-on-unmatched-pattern --check ./`);
+  }
+
+  async lintMarkdownFiles() {
+    const dockerShellCommandExecutor = await this.dockerShellCommandExecutor();
+    await dockerShellCommandExecutor.exec(`prettier --prose-wrap="always" --write --ignore-path ./.prettierignore ./`);
   }
 }
 
