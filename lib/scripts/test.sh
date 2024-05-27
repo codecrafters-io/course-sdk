fb73a6a5a6b868ac21b3d8bf6843cf9ce21528c7
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-05-27 22:03:49 +0100

    Add precompile time measurement to test.sh

--- lib/scripts/test.sh
+++ lib/scripts/test.sh
@@ -6,8 +6,13 @@ CODECRAFTERS_SUBMISSION_DIR=/app
 test -d /app-cached && cp -p -R /app-cached/. "$CODECRAFTERS_SUBMISSION_DIR"
 
 if [ -f /codecrafters-precompile.sh ]; then
+    start_time=$(date +%s%3N)
     echo ""
     /bin/sh /codecrafters-precompile.sh | sed $'s/^/\x1b[33m[compile]\x1b[0m /' || exit 1
+    echo ""
+    end_time=$(date +%s%3N)
+
+    echo "Precompile time: $((end_time - start_time)) milliseconds"
     echo ""
 fi
 
