3947e63ee9cdd83e7a4811c80189fa8e8b40d52a
Rafey Ahmad <rafeyahmad@protonmail.com>
2024-06-19 20:30:57 +0530

    Pick .codecrafters/compile.sh if present

--- lib/scripts/test.sh
+++ lib/scripts/test.sh
@@ -5,7 +5,11 @@ CODECRAFTERS_SUBMISSION_DIR=/app
 
 test -d /app-cached && cp -p -R /app-cached/. "$CODECRAFTERS_SUBMISSION_DIR"
 
-if [ -f /codecrafters-precompile.sh ]; then
+if [ -f /.codecrafters/compile.sh ]; then
+    echo ""
+    /bin/sh /.codecrafters/compile.sh | sed $'s/^/\x1b[33m[compile]\x1b[0m /' || exit 1
+    echo ""
+elif [ -f /codecrafters-precompile.sh ]; then
     echo ""
     /bin/sh /codecrafters-precompile.sh | sed $'s/^/\x1b[33m[compile]\x1b[0m /' || exit 1
     echo ""
