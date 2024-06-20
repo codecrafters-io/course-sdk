2f0643e4e7d3f402aadf8b024910eded4599a82e
Rafey Ahmad <rafeyahmad@protonmail.com>
2024-06-20 16:56:02 +0530

    Fix compile script path

--- lib/scripts/test.sh
+++ lib/scripts/test.sh
@@ -5,9 +5,9 @@ CODECRAFTERS_SUBMISSION_DIR=/app
 
 test -d /app-cached && cp -p -R /app-cached/. "$CODECRAFTERS_SUBMISSION_DIR"
 
-if [ -f /.codecrafters/compile.sh ]; then
+if [ -f "${CODECRAFTERS_SUBMISSION_DIR}/.codecrafters/compile.sh" ]; then
     echo ""
-    /bin/sh /.codecrafters/compile.sh | sed $'s/^/\x1b[33m[compile]\x1b[0m /' || exit 1
+    /bin/sh "${CODECRAFTERS_SUBMISSION_DIR}/.codecrafters/compile.sh" | sed $'s/^/\x1b[33m[compile]\x1b[0m /' || exit 1
     echo ""
 elif [ -f /codecrafters-precompile.sh ]; then
     echo ""
