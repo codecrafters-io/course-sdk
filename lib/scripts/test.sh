6df37eef1babf36e9f498857dbd3e096b4ff18f6
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-07-12 14:42:37 +0100

    Refactor test.sh script to handle parent process termination

--- lib/scripts/test.sh
+++ lib/scripts/test.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 set -e
-set -o pipefail
 
 CODECRAFTERS_SUBMISSION_DIR=/app
+
+# This is POSIX sh so we can't use pipefail. Workaround by killing parent process
+self=$$
+
+# Ensure we exit with code 1 when parent is killed using TERM
+trap 'exit 1' TERM
 
 test -d /app-cached && cp -p -R /app-cached/. "$CODECRAFTERS_SUBMISSION_DIR"
 
 if [ -f "${CODECRAFTERS_SUBMISSION_DIR}/.codecrafters/compile.sh" ]; then
     echo ""
-    /bin/sh "${CODECRAFTERS_SUBMISSION_DIR}/.codecrafters/compile.sh" | sed $'s/^/\x1b[33m[compile]\x1b[0m /' || exit 1
+    (/bin/sh "${CODECRAFTERS_SUBMISSION_DIR}/.codecrafters/compile.sh" || kill -s TERM $self) | sed $'s/^/\x1b[33m[compile]\x1b[0m /'
     echo ""
 elif [ -f /codecrafters-precompile.sh ]; then
     echo ""
-    /bin/sh /codecrafters-precompile.sh | sed $'s/^/\x1b[33m[compile]\x1b[0m /' || exit 1
+    (/bin/sh /codecrafters-precompile.sh || kill -s TERM $self) | sed $'s/^/\x1b[33m[compile]\x1b[0m /'
     echo ""
 fi
 
