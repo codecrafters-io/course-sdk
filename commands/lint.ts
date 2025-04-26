db2299b3ac871898325a81edeb8dd080453ee495
Ryan Gang <ryan-gg@outlook.com>
2025-04-26 12:13:20 +0530

    fix: update lint command to check for directory existence before running cargo clippy

--- commands/lint.ts
+++ commands/lint.ts
@@ -84,7 +84,7 @@ export default class LintCommand extends BaseCommand {
   async lintRustFiles() {
     const dockerShellCommandExecutor = await this.dockerShellCommandExecutor("rust-tools");
     await dockerShellCommandExecutor.exec(`find . -name '*.rs' -exec rustfmt --edition "2021" --check -- {} +`);
-    await dockerShellCommandExecutor.exec("cd compiled_starters/rust && cargo clippy --fix --allow-dirty");
+    await dockerShellCommandExecutor.exec("[ -d compiled_starters/rust ] && (cd compiled_starters/rust && cargo clippy --fix --allow-dirty) || exit 0");
   }
 
   async lintDockerFilesHelper(tmpDirectory: string) {
